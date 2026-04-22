9bc5c589e14ff9f8838864071864e8eef6b6eb30
LautaroJQ <quevedojoaquin@outlook.com>
2026-04-22 15:08:11 -0300

    Definicion de estructura inicial de Main

--- main.c
+++ main.c
@@ -1,10 +1,49 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include "GBT/gbt.h"
+
+//CONSTANTES
+#define TITULO "Primera Prueba"
+#define ANCHO 25
+#define ALTO 25
+#define ESCALA 15
+
+
+//ERRORES
+#define INIT_ERROR -1
+#define COLOR_ERROR -2
+#define WINDOW_ERROR -3
+
 int main()
 {
-    gbt_iniciar();
-    gbt_crear_ventana("",900,600,1);
-    printf("Hello world!\n");
+    if(gbt_iniciar() != 0){
+        printf("%s",gbt_obtener_log());
+        return INIT_ERROR;
+    }
+    //CREAMOS LA VENTANA
+    if(gbt_crear_ventana(TITULO,ANCHO,ALTO,ESCALA) != 0){
+        printf("%s",gbt_obtener_log());
+        return WINDOW_ERROR;
+    }
+
+    if(gbt_aplicar_paleta(NULL,PALETA_MAX_COLORES,GBT_FORMATO_888)!=0){
+        printf("%s",gbt_obtener_log());
+        return COLOR_ERROR;
+    }
+
+    uint8_t corriendo = 1;
+    eGBT_Tecla tecla; // Estructura tecla que detecta las teclas presionadas
+    while(corriendo)
+    {
+        gbt_procesar_entrada(); // Actualiza el estado de las teclas
+        tecla = gbt_obtener_tecla_presionada(); // Carga en la estructura tecla la tecla presionada
+        if(tecla == GBTK_ESCAPE)
+            corriendo = 0;
+
+
+
+    }
+
+
     return 0;
 }
