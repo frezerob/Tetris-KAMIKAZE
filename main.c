ab040845272d997cee87b47e5e3cb68171556a7a
LautaroJQ <quevedojoaquin@outlook.com>
2026-04-22 14:53:52 -0300

    Estructura inicial del proyecto

---
+++ main.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "GBT/gbt.h"
+int main()
+{
+    gbt_iniciar();
+    gbt_crear_ventana("",900,600,1);
+    printf("Hello world!\n");
+    return 0;
+}
